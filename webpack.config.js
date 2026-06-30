75a5d8568a96dc5d0648460eae067d55ca53499b
s selva ganesh <selvaganesh.s9629@gmail.com>
2026-07-01 00:35:12 +0530

    prepare

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -26,6 +27,12 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 
   devServer: {
     static: "./dist",
